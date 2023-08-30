31a67f28900654f1bdc7c295a76d1985ab7a5891
Kilichbek <kilich95@gmail.com>
2023-08-30 20:43:48 +0300

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    var editor = CodeMirror.fromTextArea(document.getElementById("bibtex"), {
+        lineNumbers: false,
+        lineWrapping: true,
+        readOnly:true
+    });
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    });
+});
